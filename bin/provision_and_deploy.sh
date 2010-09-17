9f9c68d48273047f39355971ce421474fd5497af
Stuart Halloway and Stuart Sierra <pair@thinkrelevance.com>
2010-09-17 13:30:19 -0400

    test deploy under new name

--- bin/provision_and_deploy.sh
+++ bin/provision_and_deploy.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-APP_NAME="zap"
+APP_NAME="mycroft"
 
 source bin/euca-functions.sh
 create_security_group $APP_NAME "22 8080 9090" &&
@@ -8,7 +8,7 @@ provision_server_with_group $APP_NAME &&
 CAP_USER=root SERVER=$EUCA_IP cap deploy:setup &&
 SERVER=$EUCA_IP cap deploy &&
 echo "--------------------- DONE --------------------"
-echo "Zap deployed to $EUCA_IP:"
+echo "Mycroft deployed to $EUCA_IP:"
 echo "   * curl http://$EUCA_IP:8080/ # to see it in action"
 echo "   * SERVER=$EUCA_IP cap deploy # to redeploy"
 echo "   * terminate_security_group_instances $APP_NAME # to terminate all running instances"
