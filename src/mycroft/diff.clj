535add458df136b2eb048583828f8bb2028463b5
Stuart Halloway <stu@thinkrelevance.com>
2010-09-30 10:12:27 -0400

    diff draft complete

--- src/mycroft/diff.clj
+++ src/mycroft/diff.clj
@@ -5,9 +5,18 @@
 (defn- atom-diff
   "Internal helper for diff."
   [a b]
-  (if (= a b)
-    [nil nil a]
-    [a b nil]))
+  (if (= a b) [nil nil a] [a b nil]))
+
+;; for big things a sparse vector class would be better
+(defn- vectorize
+  "Convert an associative-by-numeric-index collection into
+   an equivalent vector, with nil for any missing keys"
+  [m]
+  (when (seq m)
+    (reduce
+     (fn [result [k v]] (assoc result k v))
+     (vec (repeat (apply max (keys m))  nil))
+     m)))
 
 (declare diff)
 
@@ -20,54 +29,41 @@
         {:diff-similar atom-diff
          :diff-partition (fn [x] (if (.. x getClass isArray) :sequential :atom))})
 
+(defn diff-associative
+  "Diff associative things a and b, comparing only keys in ks."
+  [a b ks]
+  (reduce
+   (fn [diff1 diff2]
+     (map merge diff1 diff2))
+   [nil nil nil]
+   (map
+    (fn [k] (map #(when % {k %}) (diff (get a k) (get b k))))
+    ks)))
+
 (extend-protocol Diff
+  nil
+  (diff-partition [_] :nil)
+  (diff-similar [_ _] [nil nil nil])
+  
   java.util.Set
   (diff-partition [x] :set)
   (diff-similar [a b]
-                [(not-empty (set/difference a b))
-                 (not-empty (set/difference b a))
-                 (not-empty (set/intersection a b))])
+    [(not-empty (set/difference a b))
+     (not-empty (set/difference b a))
+     (not-empty (set/intersection a b))])
   
   java.util.Collection
   (diff-partition [x] :sequential)
   (diff-similar [a b]
-        (let [a-cnt (count a)
-              b-cnt (count b)
-              shared (min a-cnt b-cnt)
-              biggest (if (< a-cnt b-cnt) b a)]
-          (let [subdiffs (map
-                          (fn [k] (map #(when % [k %]) (diff (nth a k) (nth b k))))
-                          (range shared))] ;; slow, fix later
-            (pprint {:subdiffs subdiffs})
-            (reduce
-             (fn [diff1 diff2]
-                (map (fn [d1 [k v]] (if k (assoc d1 k v) d1)) diff1 diff2))
-             [(into (vec (repeat shared nil)) (when (> a-cnt b-cnt) (subvec a b-cnt)))
-              (into (vec (repeat shared nil)) (when (> b-cnt a-cnt) (subvec b a-cnt)))
-              (vec (repeat shared nil))] 
-             subdiffs))))
+    (vec (map vectorize (diff-associative
+                         (if (vector? a) a (vec a))
+                         (if (vector? b) b (vec b))
+                         (range (max (count a) (count b)))))))
   
   java.util.Map
   (diff-partition [x] :map)
   (diff-similar [a b]
-        (let [xkeys (set (keys a))
-              ykeys (set (keys b))
-              [only-a only-b shared] (diff xkeys ykeys)]
-          #_(pprint {:xkeys xkeys
-                   :ykeys ykeys
-                   :only-a only-a
-                   :only-b only-b})
-          (let [subdiffs (map
-                          (fn [k] (map #(when % {k %}) (diff (get a k) (get b k))))
-                          shared)]
-            #_(pprint {:subdiffs subdiffs})
-            (reduce
-             (fn [diff1 diff2]
-               (map merge diff1 diff2))
-             [(not-empty (select-keys a only-a))
-              (not-empty (select-keys b only-b))
-              nil]
-             subdiffs)))))
+    (diff-associative a b (set/union (keys a) (keys b)))))
 
 (defn diff
   "Recursively compares a and b, returning a tuple of
--- test/mycroft/diff_test.clj
+++ test/mycroft/diff_test.clj
@@ -3,9 +3,17 @@
 
 (deftest diff-test
   (are [d x y] (= d (diff x y))
+       [nil nil nil] nil nil
        [1 2 nil] 1 2
+       [nil nil [1 2 3]] [1 2 3] '(1 2 3)
+       [1 [:a :b] nil] 1 [:a :b]
+       [{:a 1} :b nil] {:a 1} :b
+       [:team #{:p1 :p2} nil] :team #{:p1 :p2}
+       [{0 :a} [:a] nil] {0 :a} [:a]
+       [nil [nil 2] [1]] [1] [1 2]
        [nil nil [1 2]] [1 2] (into-array [1 2])
        [#{:a} #{:b} #{:c :d}] #{:a :c :d} #{:b :c :d}
        [nil nil {:a 1}] {:a 1} {:a 1}
-       [{:a #{2}} {:a #{4}} {:a #{3}}] {:a #{2 3}} {:a #{3 4}}))
+       [{:a #{2}} {:a #{4}} {:a #{3}}] {:a #{2 3}} {:a #{3 4}}
+       [{:a {:c [1]}} {:a {:c [0]}} {:a {:c [nil 2] :b 1}}] {:a {:b 1 :c [1 2]}} {:a {:b 1 :c [0 2]}}))
 
