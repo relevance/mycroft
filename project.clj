4c35e70b9ebbc65fc1a42b27eeba1c8ba164c346
Jon Distad <jon.distad@gmail.com>
2010-10-25 22:08:58 -0400

    Manually patched various deps and added jars

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject mycroft "0.0.2"
   :description "It's your data"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha2"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.4.1"]
-                 [ring/ring-jetty-adapter "0.2.5"]
-                 [hiccup "0.2.6"]]
+                 [compojure "0.5.2"]
+                 [ring/ring-jetty-adapter "0.3.2"]
+                 [hiccup "0.3.0"]]
   :dev-dependencies [[autodoc "0.7.0"]
                      [jline "0.9.94"]
                      [swank-clojure "1.3.0-SNAPSHOT"]]
