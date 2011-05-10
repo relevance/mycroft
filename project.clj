ee5adac159ba421e3cf095d639d1395dea47fa11
Aaron Bedra <aaron@aaronbedra.com>
2011-05-10 19:16:47 -0400

    Updating dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
                  [org.clojure/tools.logging "0.1.2"]
                  [org.clojure/java.jmx "0.1"]
-                 [ring/ring-jetty-adapter "0.3.7"]
-                 [compojure "0.6.2"]
-                 [hiccup "0.3.4"]]
+                 [ring/ring-jetty-adapter "0.3.8"]
+                 [compojure "0.6.3" :exclusions [org.clojure/clojure]]
+                 [hiccup "0.3.5" :exclusions [org.clojure/clojure]]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]])
